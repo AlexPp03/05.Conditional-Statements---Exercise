409468bdf9d936678127ac3922b2777b10efd4e5
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-16 15:30:45 +0200

    Add files via upload

---
+++ 06. World Swimming Record/Program.cs
@@ -0,0 +1,21 @@
+﻿double recordInSeconds = double.Parse(Console.ReadLine());
+double distanceInMeters = double.Parse(Console.ReadLine());
+double oneMeterInSeconds = double.Parse(Console.ReadLine());
+
+double timeNeeded = distanceInMeters * oneMeterInSeconds;
+double delay = Math.Floor(distanceInMeters  / 15);
+double finalTime = timeNeeded + (delay * 12.5);
+
+if (finalTime < recordInSeconds)
+{
+    
+    Console.WriteLine($" Yes, he succeeded! The new world record is {finalTime:f2} seconds.");
+}
+
+else
+{
+    Console.WriteLine($"No, he failed! He was {finalTime - recordInSeconds:f2} seconds slower.");
+}
+
+    
+
