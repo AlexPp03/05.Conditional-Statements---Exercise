44365098e5f622a009cf74070d94a182e6fed676
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-16 15:28:56 +0200

    Add files via upload

---
+++ 03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,28 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+minutes = minutes + 15;
+
+int realHours = hours + minutes / 60;
+int realMinutes = minutes % 60;
+
+
+ if (realHours >= 24)
+{
+    realHours = 0;
+}
+
+
+    if (realMinutes < 10)
+{
+    Console.WriteLine($"{realHours}:0{realMinutes}");
+}
+ else 
+    {
+
+    Console.WriteLine($"{realHours}:{realMinutes}");
+}
+    
+
+    
+
+
