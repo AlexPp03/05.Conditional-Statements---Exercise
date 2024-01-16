e5f22ba0213a600a8668bd43f49fe253515c7307
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-16 15:31:25 +0200

    Add files via upload

---
+++ 07. Shopping/Program.cs
@@ -0,0 +1,25 @@
+﻿double budged = double.Parse(Console.ReadLine());
+int videoCard = int.Parse(Console.ReadLine());
+int processor = int.Parse(Console.ReadLine());
+int ramMemmory = int.Parse(Console.ReadLine());
+
+double priceForVideoCard = videoCard * 250;
+double priceForProcessor =  (priceForVideoCard  * 0.35) * processor;
+double priceForRamMemmory = (priceForVideoCard   * 0.10) * ramMemmory;
+
+double finalPrice = priceForVideoCard + priceForProcessor + priceForRamMemmory;
+
+
+if (videoCard > processor)
+{
+    finalPrice = finalPrice - finalPrice * 0.15;
+}
+
+if (budged >= finalPrice )
+{
+    Console.WriteLine($"You have {budged - finalPrice:f2} leva left!");
+}
+else
+{
+    Console.WriteLine($"Not enough money! You need {finalPrice - budged:f2} leva more!");
+}
