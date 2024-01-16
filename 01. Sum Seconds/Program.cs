be81c29c7933452203f19a67c346069fee2b40a3
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-16 15:27:44 +0200

    Add files via upload

---
+++ 01. Sum Seconds/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstTime = int.Parse(Console.ReadLine());
+int secondTime = int.Parse(Console.ReadLine());
+int thirdTime = int.Parse(Console.ReadLine());
+
+int time = firstTime + secondTime + thirdTime;
+
+double minutes = time / 60;
+double seconds = time % 60;
+
+if (seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+
+}
+else
+
+Console.WriteLine($"{minutes}:{seconds}");
