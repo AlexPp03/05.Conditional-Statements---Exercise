5e4440a8079ed8352dbdacd699f75ce77a075bb5
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-16 15:29:58 +0200

    Add files via upload

---
+++ 05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,26 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int extra = int.Parse(Console.ReadLine());
+double priceForOneExtra = double.Parse(Console.ReadLine());
+
+double decor = budget * 0.10;
+double priceForClothes = priceForOneExtra * extra;
+
+
+if (extra > 150)
+{
+    priceForClothes = priceForClothes - priceForClothes * 0.10;
+
+}
+
+double sum = decor + priceForClothes;
+
+if (sum > budget)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {sum - budget:f2} leva more.");
+}
+else if (sum <= budget)
+{
+    Console.WriteLine("Action!"); 
+    Console.WriteLine($"Wingard starts filming with {budget - sum :f2} leva left.");
+}
