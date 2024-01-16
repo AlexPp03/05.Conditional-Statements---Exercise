f769b8341c2aee0aa8567b5013eb0be167a8ac69
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-16 15:31:56 +0200

    Add files via upload

---
+++ 08. Lunch Break/Program.cs
@@ -0,0 +1,17 @@
+﻿string name = Console.ReadLine();
+int timeOfEpisode = int.Parse(Console.ReadLine());
+int restTime = int.Parse(Console.ReadLine());
+
+double lunchTime = restTime / 8.0;
+double timeRest = restTime / 4.0;
+double fullRest = lunchTime + timeRest;
+double timeleft = restTime - fullRest;
+
+if (timeleft >= timeOfEpisode)
+{
+    Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(timeleft - timeOfEpisode)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(timeOfEpisode - timeleft)} more minutes.");
+}
